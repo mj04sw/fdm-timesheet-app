e1e674d534fa088d1944a52214b9876da418cd92
mj04sw <mathu04dec@proton.me>
2025-03-21 18:24:11 +0000

    MJ- Edited the styling to get rid of the react and vite logos, the random ahh counter and the read the docs section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,16 @@
 export default function Home() {
     return (
-      <div>
+      <div className="hero">
         <h1>Welcome to FDM Timesheet App</h1>
         <p>Dashboard and overview go here.</p>
+        <div className="cta">
+          {/* Make these links into buttons */}
+          <button className="cta-btn" onClick={() => window.location.href = "/"}>Home</button>
+          <button className="cta-btn" onClick={() => window.location.href = "/login"}>Login</button>
+          <button className="cta-btn" onClick={() => window.location.href = "/timesheet"}>Timesheet</button>
+          <button className="cta-btn" onClick={() => window.location.href = "/admin"}>Admin Panel</button>
+        </div>
       </div>
     );
   }
+  
